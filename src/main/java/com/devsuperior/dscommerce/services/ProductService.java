82cdf0221d8488adba4d4733c456abf058b291e3
thiago sousa <thiagosohsa@gmail.com>
2026-02-06 11:47:11 -0300

    Implement ProductService and method to fetch product by ID

---
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -0,0 +1,33 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    // Versão resumida
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Product product = repository.findById(id).get();
+        return new ProductDTO(product);
+
+
+/*
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Optional<Product> result = repository.findById(id);
+        Product product = result.get();
+        ProductDTO dto = new ProductDTO(product);
+        return dto;
+    }
+ */
+    }
+}
